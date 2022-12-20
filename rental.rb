16814ed2d705421c7a2b29111b7bc85972e31c72
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-12-20 15:40:32 +0000

    Fix rubocop errors

--- rental.rb
+++ rental.rb
@@ -1,11 +1,11 @@
 class Rental
-    attr_reader :book, :person, :date
-  
-    def initialize(book, person, date)
-      @book = book
-      @person = person
-      @date = date
-      book.rentals << self
-      person.rentals << self
-    end
+  attr_reader :book, :person, :date
+
+  def initialize(book, person, date)
+    @book = book
+    @person = person
+    @date = date
+    book.rentals << self
+    person.rentals << self
   end
+end
