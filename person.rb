d27046f40054f15a5f69647af43095310637dd02
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-12-20 15:36:57 +0000

    Create relationship btw book,rental and car

--- person.rb
+++ person.rb
@@ -1,9 +1,11 @@
 require './nameable'
 require './capitalize_decorator'
 require './trimmer_decorator'
+require './book'
+require './rental'
 
 class Person < Nameable
-  attr_reader :id
+  attr_reader :id, :rentals
   attr_accessor :name, :age
 
   def initialize(age, name = 'Unknown', parent_permission: true)
@@ -12,6 +14,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   private
