11ad59139189e574763d71d549620cdc5e93c10b
Didier Peran Ganthier <didierganthierperan@gmail.com>
2023-01-04 21:38:52 +0000

    Write teacher test

--- teacher.rb
+++ teacher.rb
@@ -1,6 +1,8 @@
 require './person'
 
 class Teacher < Person
+  attr_reader :specialization
+
   def initialize(age, specialization, name, parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @specialization = specialization
